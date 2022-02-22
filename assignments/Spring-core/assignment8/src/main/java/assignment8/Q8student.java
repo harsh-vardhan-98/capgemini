fdcc32072276c37ba3bdf5d0f0bba9ec8a446b2b
harsh-vardhan-98 <harshkaurav37@gmail.com>
2022-02-22 14:19:23 +0530

    Question 8 of spring core

---
+++ assignments/Spring-core/assignment8/src/main/java/assignment8/Q8student.java
@@ -0,0 +1,43 @@
+package assignment8;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+class Student{
+
+    String name;
+
+ 
+    public String getName() {
+        return name;
+    }
+    public void getDetails()
+    {
+        System.out.println("the name of the student is: "+getName());
+    }
+    @PostConstruct
+    public void myInit()
+    {
+        System.out.println("myInit method started");
+    }
+    @PreDestroy
+    public void myDestroy()
+    {
+        System.out.println("myInit method started");
+    }
+}
+
+
+public class Q8student {
+	 public static void main(String[] args) {
+	        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+	        ((ConfigurableApplicationContext) context).registerShutdownHook();
+	        Student obj = (Student) context.getBean("student");
+	        obj.getDetails();
+	    }
+}
+
